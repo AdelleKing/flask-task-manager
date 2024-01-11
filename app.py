b67b4cb5d2e63880614f6f087c72fd3f06ca64ef
Adelle King <adelle_xx@yahoo.co.uk>
2024-01-11 16:42:33 +0000

    added session user profile

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def register():
         #put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for('profile', username=session["user"]))
     return render_template("register.html")
 
 
@@ -66,6 +67,7 @@ def login():
                 exisiting_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
+                    return redirect(url_for('profile', username=session["user"]))
             else:
                 #invalid password match
                 flash("Incorrect Username and/or Password")
@@ -78,8 +80,13 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/profile/<username>", methods=["GET","POST"])
+def profile(username):
+    #getting the users username from the database
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
-    return render_template("login.html")
 
 
 
